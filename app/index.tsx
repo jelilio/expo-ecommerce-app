3069950856e0aa98dfd99e293e9c0cb853a7e5b7
Jelili Adesina <jelilio.adesina@gmail.com>
2025-01-01 21:43:44 +0000

    hide the header

--- app/index.tsx
+++ app/index.tsx
@@ -1,24 +1,27 @@
 import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import React from "react";
-import { Link } from "expo-router";
+import { Link, Stack } from "expo-router";
 
 type Props = {};
 
 const WelcomeScreen = (props: Props) => {
   return (
-    <View style={styles.container}>
-      <Text>Welcome Screen</Text>
-      <Link href={"/signin"} asChild>
-        <TouchableOpacity>
-          <Text>Go to SignIn Screen</Text>
-        </TouchableOpacity>
-      </Link>
-      <Link href={"/signup"} asChild>
-        <TouchableOpacity>
-          <Text>Go to SignUp Screen</Text>
-        </TouchableOpacity>
-      </Link>
-    </View>
+    <>
+      <Stack.Screen options={{ headerShown: false }} />
+      <View style={styles.container}>
+        <Text>Welcome Screen</Text>
+        <Link href={"/signin"} asChild>
+          <TouchableOpacity>
+            <Text>Go to SignIn Screen</Text>
+          </TouchableOpacity>
+        </Link>
+        <Link href={"/signup"} asChild>
+          <TouchableOpacity>
+            <Text>Go to SignUp Screen</Text>
+          </TouchableOpacity>
+        </Link>
+      </View>
+    </>
   );
 };
 
